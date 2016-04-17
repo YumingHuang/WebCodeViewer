2081aadf27aa850a8c9257a44e3db24c88df2375
Ming <apple@MacBook-Air.local>
2016-04-17 22:23:49 +0800

    Initial commit

---
+++ app/src/main/java/com/wangjialin/internet/service/HtmlService.java
@@ -0,0 +1,22 @@
+package com.wangjialin.internet.service;
+
+import com.wangjialin.internet.utils.StreamTool;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class HtmlService {
+
+	public static String getHtml(String path) throws Exception {
+		HttpURLConnection conn = (HttpURLConnection)new URL(path).openConnection();
+		conn.setConnectTimeout(5000);
+		conn.setRequestMethod("GET");
+		if(conn.getResponseCode() == 200){
+			InputStream inStream = conn.getInputStream();
+			byte[] data = StreamTool.read(inStream);
+			return new String(data);
+		}
+		return null;
+	}
+}
